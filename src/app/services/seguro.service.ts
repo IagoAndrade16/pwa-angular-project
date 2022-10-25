5e300cd1a9917a767ed7f195a8d5fa283e9eec4d
Iago Alexandre <iagoaap@outlook.com>
2022-10-25 08:07:35 -0300

    implemented conection status verification

--- src/app/services/seguro.service.ts
+++ src/app/services/seguro.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Seguro } from '../models/Seguro';
+import { OnlineOfflineService } from './online-offline.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,27 @@ export class SeguroService {
 
 
   constructor(
-    private http: HttpClient
-  ) { }
+    private http: HttpClient,
+    private onlineOfflineService: OnlineOfflineService
+  ) { 
+    this.ouvirStatusConexao();
+  }
+
+  private salvarAPI(seguro: Seguro) {
+    this.http.post(this.API_SEGUROS + '/api/seguros', seguro)
+      .subscribe(
+        () => alert('Seguro cadastrado com sucesso'),
+        (err) => console.log(err)
+      )
+  }
+
+  public salvar(seguro: Seguro) {
+    if(this.onlineOfflineService.isOnline) {
+      this.salvarAPI(seguro)
+    } else {
+      console.log('Salvar seguro no banco local')
+    }
+  }
 
   cadastrar(seguro: Seguro) {
     this.http.post(this.API_SEGUROS + '/api/seguros', seguro)
@@ -25,5 +45,16 @@ export class SeguroService {
 
   listar(): Observable<Seguro[]> {
     return this.http.get<Seguro[]>(this.API_SEGUROS + '/api/seguros');
+  }
+
+  private ouvirStatusConexao() {
+    this.onlineOfflineService.statusConexao
+      .subscribe(online => {
+        if(online) {
+          console.log('enviando os dados do meu banco local para API')
+        } else {
+          console.log('Esotu offline')
+        }
+      })
   }
 }
