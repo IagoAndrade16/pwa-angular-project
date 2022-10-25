a0bfc0836acca93a3a57e8bcc19f476832dd33e9
Iago Alexandre <iagoaap@outlook.com>
2022-10-25 07:11:34 -0300

    build frontend of application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { MenuComponent } from './components/menu/menu.component';
+import { CadastroSeguroComponent } from './components/cadastro-seguro/cadastro-seguro.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientJsonpModule } from '@angular/common/http';
+import { ListarSegurosComponent } from './components/listar-seguros/listar-seguros.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CadastroSeguroComponent,
+    ListarSegurosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
---
+++ src/app/models/Seguro.ts
@@ -0,0 +1,10 @@
+import { MarcaCarro } from "./marcaCarro";
+
+export class Seguro {
+    marcaCarro!: MarcaCarro;
+    nomeProprietário!: string;
+    modeloCarro!: string;
+    sobrenomeProprietario!: string;
+    placaCarro!: string;
+    dataNascimentoProprietario!: string;
+}
